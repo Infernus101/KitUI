d7b276f653faaa78d3cf20f6aefb0f865b875038
Infernus <Infernus101@users.noreply.github.com>
2017-12-28 23:46:08 +0530

    Remove removing money before adding kit

--- src/Infernus101/KitUI/UI/windows/KitInfo.php
+++ src/Infernus101/KitUI/UI/windows/KitInfo.php
@@ -69,15 +69,6 @@ class KitInfo extends Window {
 				$this->navigate(Handler::KIT_ERROR, $this->player, $windowHandler);
 				break;
 			}
-			if(isset($kits->data["money"])){
-				$money = $kits->data["money"];
-				if(EconomyAPI::getInstance()->reduceMoney($this->player, $money) === EconomyAPI::RET_INVALID){
-					$error = $this->pl->language->getTranslation("cant-afford", $name, $money);
-					$this->pl->id[strtolower($this->player->getName())]["error"] = $error;
-					$this->navigate(Handler::KIT_ERROR, $this->player, $windowHandler);
-					break;
-				}
-			}
 			if(($this->pl->config->get("one-kit-per-life")) and (isset($kits->pl->kitused[strtolower($this->player->getName())]))){
 				$error = $this->pl->language->getTranslation("one-per-life");
 				$this->pl->id[strtolower($this->player->getName())]["error"] = $error;
