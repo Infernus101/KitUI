3b80bfe7979cc566ed7d0ce4a7cbd53fc5e9c0f6
Infernus <Infernus101@users.noreply.github.com>
2017-12-29 00:33:09 +0530

    Update KitInfo.php

--- src/Infernus101/KitUI/UI/windows/KitInfo.php
+++ src/Infernus101/KitUI/UI/windows/KitInfo.php
@@ -63,7 +63,8 @@ class KitInfo extends Window {
 				break;
 			}
 			if(isset($kits->data["money"])){
-				if(EconomyAPI::getInstance()->myMoney($this->player) < $kits->data["money"]){
+				$money = $kits->data["money"];
+				if(EconomyAPI::getInstance()->myMoney($this->player) < $money){
 					$error = $this->pl->language->getTranslation("cant-afford", $name, $money);
 					$this->pl->id[strtolower($this->player->getName())]["error"] = $error;
 					$this->navigate(Handler::KIT_ERROR, $this->player, $windowHandler);
