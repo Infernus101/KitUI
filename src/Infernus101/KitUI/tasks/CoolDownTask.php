fc7a0d0904448a1d28ebe8ccace8cbf76a7f28a8
Infernus <Infernus101@users.noreply.github.com>
2018-07-15 01:28:22 +0530

    plugintask -> task

--- src/Infernus101/KitUI/tasks/CoolDownTask.php
+++ src/Infernus101/KitUI/tasks/CoolDownTask.php
@@ -3,14 +3,13 @@
 namespace Infernus101\KitUI\tasks;
 
 use Infernus101\KitUI\Main;
-use pocketmine\scheduler\PluginTask;
+use pocketmine\scheduler\Task;
 
-class CoolDownTask extends PluginTask{
+class CoolDownTask extends Task{
 
     private $plugin;
 
     public function __construct(Main $plugin){
-        parent::__construct($plugin);
         $this->plugin = $plugin;
     }
 
